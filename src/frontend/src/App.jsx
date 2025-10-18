12ba3355150132a3b8fe2f74f296675a3c67e1bf
thisYui <160146960+thisYui@users.noreply.github.com>
2025-10-18 15:55:26 +0700

    add palette

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -8,20 +8,29 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/*<Route path="/" element={<IndexPage />} />*/}
-        {/*<Route path="/login" element={<LoginPage />} />*/}
-        {/*<Route path="/signup" element={<SignupPage />} />*/}
-        {/*<Route path="/confirm_otp" element={<ConfirmOTPPage />} />*/}
-        {/*<Route path="/settings" element={<SettingPage />} />*/}
-        {/*<Route path="/post" element={<CreatePostPage />} />// create*/}
-        {/*<Route path="/post/:id" element={<PostPage />} />*/}
-        {/*<Route path="/repost/:id" element={<RepostPage />} />*/}
-        {/*<Route path="/notebook" element={<NotebookPage />} />*/}
-        {/*<Route path="/question" element={QuestionPage />} />*/}
-        {/*<Route path="/search" element={<SearchPage />} />*/}
-        {/*<Route path="/cook" element={<CookPage />} /> step by step*/}
-        {/*<Route path="/analytics" element={<AnalyticsPage />} />*/}
-        {/*<Route path="/ai" element={<AIPage />} />*/}
+          {/*<Route element={<AuthLayout />}>*/}
+          {/*    /!*<Route path="/login" element={<LoginPage />} />*!/*/}
+          {/*    /!*<Route path="/signup" element={<SignupPage />} />*!/*/}
+          {/*    /!*<Route path="/confirm_otp" element={<ConfirmOTPPage />} />*!/*/}
+          {/*</Route>*/}
+
+          {/*<Route element={<MainLayout />}>*/}
+          {/*    /!*<Route path="/settings" element={<SettingPage />} />*!/*/}
+          {/*    /!*<Route path="/post" element={<CreatePostPage />} />// create*!/*/}
+          {/*    /!*<Route path="/post/:id" element={<PostPage />} />*!/*/}
+          {/*    /!*<Route path="/repost/:id" element={<RepostPage />} />*!/*/}
+          {/*    /!*<Route path="/notebook" element={<NotebookPage />} />*!/*/}
+          {/*    /!*<Route path="/question" element={QuestionPage />} />*!/*/}
+          {/*    /!*<Route path="/search" element={<SearchPage />} />*!/*/}
+          {/*    /!*<Route path="/analytics" element={<AnalyticsPage />} />*!/*/}
+          {/*    /!*<Route path="/ai" element={<AIPage />} />*!/*/}
+          {/*    /!*<Route path="/" element={<IndexPage />} />*!/*/}
+          {/*</Route>*/}
+
+          {/*<Route element={<FullLayout />}>*/}
+          {/*  /!*<Route path="/cook" element={<CookPage />} /> step by step*!/*/}
+          {/*  /!*<Route path="*" element={<NotFoundPage />} />*!/*/}
+          {/*</Route>*/}
       </Routes>
     </BrowserRouter>
   )
