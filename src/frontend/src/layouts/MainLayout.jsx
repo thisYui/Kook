7d5adf4cb524fc1b20ab7ba066c3e230d0e87bc6
chuhieumagic <23120258@student.hcmus.edu.vn>
2025-10-20 22:24:18 +0700

    hotfix

--- src/frontend/src/layouts/MainLayout.jsx
+++ src/frontend/src/layouts/MainLayout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
-import NavbarLogin from "../components/NavbarLogin";
-import NavbarLogout from "../components/NavbarLogout"
+import Navbar from '../components/NavigationBar';
+
 export default function MainLayout() {
   return (
     <div className="flex">
       {/* <NavbarLogin /> */}
-      <NavbarLogout />
+      <Navbar />
       <main className="flex-1 ml-64 p-6"> {/* ml-64 để chừa chỗ cho Navbar */}
         <Outlet /> {/* nơi render page con */}
       </main>
