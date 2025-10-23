ee8ebdaeb726efe54f0194adfe376426fdf7caeb
chuhieumagic <23120258@student.hcmus.edu.vn>
2025-10-24 00:32:45 +0700

    Change login/logout bg to link instead of file to improve image quality

--- src/frontend/src/layouts/AuthLayout.jsx
+++ src/frontend/src/layouts/AuthLayout.jsx
@@ -1,10 +1,9 @@
 import { Outlet } from "react-router-dom";
-import loginBg from "../assets/login_bg.jpg";
 
 export default function AuthLayout() {
     return (
         <div className="relative h-screen flex items-center justify-center">
-        <img src={loginBg} alt="Background" className="w-full h-full object-cover absolute top-0 left-0 z-0" />
+        <img src="https://images.pexels.com/photos/1565982/pexels-photo-1565982.jpeg" alt="Background" className="w-full h-full object-cover absolute top-0 left-0 z-0" />
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-md bg-white p-8 rounded-lg shadow-xl z-10">
             <Outlet />
         </div>
